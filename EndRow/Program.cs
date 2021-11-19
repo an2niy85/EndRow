faf12c3f4c5e318e551595a1dc8cfad93cee4bd4
Антон Красноперов <an2niy85@mail.ru>
2021-11-19 16:41:08 +0400

    Добавьте файлы проекта.

---
+++ EndRow/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndRow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myRow = "";
+
+            for (int i = 7; i < 100; i+=7)
+            {
+                myRow = myRow + " " +i; 
+            }
+
+            Console.WriteLine(myRow);
+            Console.ReadLine();
+        }
+    }
+}
